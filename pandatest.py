a57071a3a897cac1c88246d25ac13fad25fe540b
Hakan-Fridlund <garonneman79@gmail.com>
2026-09-09 07:01:22 +0200

    updated pandastest for english descriptions also going through warnings and spell checking

--- pandatest.py
+++ pandatest.py
@@ -1,28 +1,29 @@
 """
-Ladda CSV‑filen i Pandas, titta på strukturen, räkna statistik (mean, median, std),
-filtrera fram olika species och värden, och gör några enkla visualiseringar.
-Det här blir din första riktiga EDA (Exploratory Data Analysis).
-1   Utforska datans struktur --- df.info() → datatyper, null‑värden
-df.describe() → statistik (mean, std, min, max),   df.shape → antal rader/kolumner
-df["species"].value_counts() → 50 av varje art (setosa, versicolor, virginica)
-2   Räkna statistik per kolumn, median, mean, corr,
-3   Filtrera data ---  Alla setosa:,  Petal length > 5:, Kombinerade villkor:
-4   Gruppbaserad statistik, Räkna statistik per art: df.groupby("species").agg(["mean","std","min","max"])
-5   Visualiseringar (valfritt men rekommenderat)   Du kan använda matplotlib eller seaborn.
+assignment: load csv-file in pandas. look at the structure, count statistics (mean, median,std)
+filter different species and values and make a few simple vizualisations.
+EDA (Exploratory Data Analysis).
+1 explore the data structure --- df.info() → datatypes, null‑values
+df.describe() → statistics (mean, std, min, max),   df.shape → numer of rows/columns
+df["species"].value_counts() → 50 of every species (setosa, versicolor, virginica)
+2   count statistics per column, median, mean, corr.
+3   Filter data ---  All setosa:,  Petal length > 5:, combined conditions:
+4   Groupbased statistics, count statistics per species: df.groupby("species").agg(["mean","std","min","max"])
+5   Vizualisations (optional but recommended) Use matplotlib or seaborn
 """
 import matplotlib.pyplot as plt
 import pandas as pd
-df = pd.read_csv("https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv")
+# noinspection PyArgumentList
+df =  pd.read_csv("https://raw.githubusercontent.com/uiuc-cse/data-fa14/gh-pages/data/iris.csv")
 
-#print(df.select_dtypes(include="number").corr())  # för att sortera bort STR kolumner vid CORR
-#print(df[(df["species"] == "setosa") & (df["petal_length"] > 1.6)]) #kombinerar villkor vid filtrering
+#print(df.select_dtypes(include="number").corr())  # to sort away STR columns by CORR
+#print(df[(df["species"] == "setosa") & (df["petal_length"] > 1.6)]) #combines conditions by filtering
 #print (df.groupby("species").agg(["std"]))
 
 df.info()
 #df1 = df.groupby(["species"]).mean()
 #print (df1)
 #bars = plt.bars(df1.index, df1.sepal_length)
-
+# noinspection PyArgumentList
 df.boxplot(column="sepal_length",by="species")
 
 plt.xlabel("species")
